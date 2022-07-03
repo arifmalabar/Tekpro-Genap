a6c300f66200712ba6085a24c754895ebbf95d8d
Ridho Arif Wicaksono <56249846+arifmalabar@users.noreply.github.com>
2022-07-04 05:43:46 +0700

    new

---
+++ src/tekproma/TekpromA.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tekproma;
+
+import gui.TesGui;
+import tugas_penilaian.Nim2118055_Penilaian;
+
+/**
+ *
+ * @author ridho
+ */
+public class TekpromA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nim2118055_Penilaian nilai = new Nim2118055_Penilaian();
+        nilai.setVisible(true);
+    }   
+}
